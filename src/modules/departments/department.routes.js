949656dc64b8ef7b3f7afc678e0a1c5b10278290
eng-mohamed-I <mohamedyasiiin1@gmail.com>
2024-09-20 21:24:38 +0300

    fix some bugs

--- src/modules/departments/department.routes.js
+++ src/modules/departments/department.routes.js
@@ -11,9 +11,9 @@ import {
 const departmentRoutes = Router();
 
 departmentRoutes.post("/create", createDepartment);
-departmentRoutes.get("/get-all", getAllDepartments);
-departmentRoutes.get("/get-one/:id", getDepartmentById);
-departmentRoutes.put("/update/:id", updateDepartment);
-departmentRoutes.delete("/delete/:id", deleteDepartment);
+departmentRoutes.get("/", getAllDepartments);
+departmentRoutes.get("/:id", getDepartmentById);
+departmentRoutes.put("/:id", updateDepartment);
+departmentRoutes.delete("/:id", deleteDepartment);
 
 export default departmentRoutes;
