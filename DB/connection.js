64c746c0bb82e3fb7563e54f8a9759b6df9638e8
andf.750@gmail.com <andf.750@gmail.com>
2024-09-21 19:56:28 +0300

    to pull

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
-// const uri ="mongodb+srv://eslam:eslam@hospital.q8ghh.mongodb.net/?retryWrites=true&w=majority&appName=test";
-const uri = "mongodb://localhost:27017/backend-graduation-project";
+const uri ="mongodb+srv://eslam:eslam@hospital.q8ghh.mongodb.net/?retryWrites=true&w=majority&appName=test";
+// const uri = "mongodb://localhost:27017/backend-graduation-project";
 export const connectionDB = mongoose
   .connect(uri)
   .then(() => {
